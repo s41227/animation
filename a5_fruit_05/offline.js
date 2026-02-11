9511e778a7722171283b9a40f80bdedfa7cd023c
s41227 <s41227@bj.ac.th>
2026-02-11 11:11:34 +0700

    Add files via upload

---
+++ a5_fruit_05/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1770782956,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/botcut-sheet0.png",
+		"images/fruitslash-sheet0.png",
+		"images/grapebot-sheet0.png",
+		"images/grapetop-sheet0.png",
+		"images/grapew-sheet0.png",
+		"images/strawberry-sheet0.png",
+		"images/topcut-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
